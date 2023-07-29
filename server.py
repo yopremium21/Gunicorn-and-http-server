f79801ca0af7a0b79bea4a720dfeab25245432e7
yopremium21 <121346873+yopremium21@users.noreply.github.com>
2023-07-29 09:09:40 +0530

    HTTP-Server

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-def app(environ, start_response):
-        data = b"Server Running..!\n"
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-        ])
-        return iter([data])
+import http.server
+import socketserver
+
+PORT = 8080
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = MyHttpRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Http Server Serving at port", PORT)
+    httpd.serve_forever()
